f17594f357f7283b3a68981524b8d3e80a323029
P1anetA <natkoch09@gmail.com>
2022-12-17 23:58:45 +0300

    os-feat: sort by price implemented

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import {renderCards} from './script/store-page';
 import {createCheckbox} from './script/create-filter';
+import {sortCards} from './script/sort';
 
 const categoryPath = <HTMLDivElement>document.querySelector('.category');
 const brandPath = <HTMLDivElement>document.querySelector('.brand');
+const sortBtns = document.querySelectorAll('.dropdown-item');
+
+sortBtns.forEach(element => {
+    element.addEventListener('click', (event) => {
+        if ((event.target as Element).classList.contains('price')) {
+            console.log('price')
+            sortCards('.price', 'asc');
+        }
+    })
+});
 
 fetch('https://dummyjson.com/products?limit=100')
     .then(response => response.json())
--- src/script/store-page.ts
+++ src/script/store-page.ts
@@ -4,7 +4,7 @@ export function renderCards(data: ICard[]) {
     const CARDS_BOX = <HTMLDivElement>document.querySelector('.items-cards');
     for (let i = 0; i < data.length; i++){
         const finalPrice = ((data[i].price / 100) * (100 - data[i].discountPercentage)).toFixed(1);
-        const card = `<div class="card h-100" data-id = '${data[i].id}'>
+        const card = `<div class="card h-100" data-id = '${data[i].id}' data-price = "${data[i].price}">
                   <img src="${data[i].thumbnail}" class="img-thumbnail" alt="Card image">
                   <div class="card-body">
                     <h5 class="card-title">${data[i].title}</h5>
@@ -17,9 +17,9 @@ export function renderCards(data: ICard[]) {
                     <span class="stock">${data[i].stock}</span>
                     </p>
                     <p class="card-text text-end">
-                      <span class="price">€${data[i].price}</span>
+                      <span class="price"">€${data[i].price}</span>
                       <small class="text-muted sale">-${data[i].discountPercentage}%</small>
-                      <span class="final-price h3">€${finalPrice}</span>
+                      €<span class="final-price h3">${finalPrice}</span>
                     </p>
                   </div>
                 </div>`;
