e5788894ed5437df418dfdfe3ee4aa099e40087a
P1anetA <natkoch09@gmail.com>
2022-12-17 17:38:48 +0300

    os-refactor: optimize createFilter function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import {renderCards} from './script/store-page';
-import {createFilter} from './script/create-filter';
+import {createCheckbox} from './script/create-filter';
+
+const categoryPath = <HTMLDivElement>document.querySelector('.category');
+const brandPath = <HTMLDivElement>document.querySelector('.brand');
 
 fetch('https://dummyjson.com/products?limit=100')
     .then(response => response.json())
     .then(data => {
         renderCards(data.products);
-        createFilter(data.products);
+        createCheckbox(data.products, 'category', categoryPath);
+        createCheckbox(data.products, 'brand', brandPath);
     });
