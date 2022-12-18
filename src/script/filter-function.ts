483c54b7c72badf29e48f494440fc1f0ca5e3aad
Anton Haldouski <anton94-94@list.ru>
2022-12-18 21:26:03 +0300

    os-feat: started creating dual-slider input filter (noUiSlider)

--- src/script/filter-function.ts
+++ src/script/filter-function.ts
@@ -1,60 +1,76 @@
 import {defaultData} from '../index';
 import {renderCards, CARDS_BOX} from './store-page';
 import {ICard} from "../types";
+import noUiSlider from 'nouislider';
+
 
 export function filterData(): void {
-    /*    const inputPriceRange = <HTMLInputElement>document.querySelector('#price-range');
-        const inputStockRange = <HTMLInputElement>document.querySelector('#stock-range');*/
-    const categories: NodeListOf<HTMLInputElement> = document.querySelectorAll('.accordion-body.category input');
-    const brands: NodeListOf<HTMLInputElement> = document.querySelectorAll('.accordion-body.brand input');
+    const CATEGORIES: NodeListOf<HTMLInputElement> = document.querySelectorAll('.accordion-body.category input');
+    const BRANDS: NodeListOf<HTMLInputElement> = document.querySelectorAll('.accordion-body.brand input');
+/*    const PRICE_MIN = <HTMLSpanElement>document.querySelector('.price-range>.text-start>.min-price');
+    const PRICE_MAX = <HTMLSpanElement>document.querySelector('.price-range>.text-end>.max-price');
+    const STOCK_MIN = <HTMLDivElement>document.querySelector('.stock-range>.text-start');
+    const STOCK_MAX = <HTMLDivElement>document.querySelector('.stock-range>.text-end');*/
 
-    function elemEvent(this: HTMLInputElement): void {
-        const elemId: string = this.id;
-        const checkedCategories: string[] = [...document.querySelectorAll('.accordion-body.category input:checked')].map(item => item.id);
-        const checkedBrands: string[] = [...document.querySelectorAll('.accordion-body.brand input:checked')].map(item => item.id);
+    const SLIDER_PRICE = <HTMLDivElement>document.getElementById('slider-price');
+    noUiSlider.create(SLIDER_PRICE, {
+        start: [0, 2000],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 2000
+        },
+        step: 10,
+    });
+
+    const SLIDER_STOCK = <HTMLDivElement>document.getElementById('slider-stock');
+    noUiSlider.create(SLIDER_STOCK, {
+        start: [0, 200],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 200
+        },
+        step: 1,
+    });
+
+    // const SLIDER = <HTMLDivElement>document.querySelector('.noUi-handle .noUi-handle-lower');
+    // console.log(SLIDER)
+
+    function elemEvent(/*this: HTMLInputElement*/): void {
+        // const elemId: string = this.id;
+        const checkedCategories: string[] = [...document.querySelectorAll('.accordion-body.category input:checked')]
+            .map(item => item.id);
+        const checkedBrands: string[] = [...document.querySelectorAll('.accordion-body.brand input:checked')]
+            .map(item => item.id);
+        // const value = SLIDER_PRICE.noUiSlider.getPositions();
+
         let filteredData: ICard[];
-        if(checkedBrands.length === 0 && checkedCategories.length === 0){
+        if (checkedBrands.length === 0 && checkedCategories.length === 0) {
             filteredData = defaultData.map(item => item);
+            // filteredData = filteredData.filter(item => item.price <= );
+        } else if (checkedBrands.length > 0 && checkedCategories.length > 0) {
+            filteredData = defaultData.filter(item => (checkedCategories.includes(item.category) &&
+                checkedBrands.includes(item.brand)));
+        } else {
+            filteredData = defaultData.filter(item => (checkedCategories.includes(item.category) ||
+                checkedBrands.includes(item.brand)));
         }
-        else if (checkedBrands.length > 0 && checkedCategories.length > 0){
-            filteredData = defaultData.filter(item => (
-                (checkedCategories.includes(item.category) && checkedBrands.includes(item.brand))
-            ));
-        }else {
-            filteredData = defaultData.filter(item => (
-                (checkedCategories.includes(item.category) || checkedBrands.includes(item.brand))
-            ));
-        }
+
 
         CARDS_BOX.innerHTML = '';
-        renderCards(filteredData);
+        if (filteredData.length === 0) {
+            CARDS_BOX.innerHTML = `<p class="no-products">No products found</p>`;
+        } else {
+            renderCards(filteredData);
+        }
     }
 
-    categories.forEach(elem => {
+    CATEGORIES.forEach(elem => {
         elem.addEventListener('click', elemEvent);
     });
 
-    brands.forEach(elem => {
+    BRANDS.forEach(elem => {
         elem.addEventListener('click', elemEvent);
     });
 }
-
-/*
-function filterGoods() {
-    const
-        country = filters.querySelector('#country').value,
-        sizes = [...filters.querySelectorAll('#size input:checked')].map(n => n.value),
-        priceMin = document.querySelector('#price-min').value,
-        priceMax = document.querySelector('#price-max').value;
-
-    outputGoods(DATA.filter(n => (
-        (!country || n.country === country) &&
-        (!sizes.length || sizes.includes(n.size)) &&
-        (!priceMin || priceMin <= n.cost) &&
-        (!priceMax || priceMax >= n.cost)
-    )));
-}
-*/
-
-
-
